2432919a0214a4990c06c0bcd598db8940fc5771
Dang Tinh <>
2023-03-14 00:33:26 +0700

    fix docker

--- app/Services/YoutubeService.php
+++ app/Services/YoutubeService.php
@@ -22,7 +22,7 @@ class YoutubeService
      */
     public function listVideo(string $token): ApiResponse
     {
-        $url = ELinkYoutube::BASE_URL->value;
+        $url = ELinkYoutube::BASE_URL->value.'/index?gl=VN';
         $body = Http::withHeaders([
             'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
         ])->get($url)->body();

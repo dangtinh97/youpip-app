d2e4c47a321165f3252d68e936ba6ebbbc77b64c
Dang Tinh <>
2023-04-07 19:56:57 +0700

    logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,15 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/x-icon" href="{{asset('logo-512.ico')}}">
+    <meta name="keywords" content="YouPip, Youtube không quảng cáo, Youtube">
+    <meta name="description" content="Xem video không quảng cáo,xem video trong nền, chat cùng openai">
+    <meta name="author" content="YouPiP">
+    <meta property="og:url"                content="https://youpip.net" />
+    <meta property="og:type"               content="Video" />
+    <meta property="og:title"              content="#YouPip Xem video không quảng cáo" />
+    <meta property="og:description"        content="Xem video không quảng cáo,xem video trong nền, chat cùng openai" />
+    <meta property="og:image"              content="https://youpip.net/img/logo-512.jpg" />
+
     <title>YouPiP</title>
     <style>
         body {
@@ -35,17 +44,6 @@
 <div id="app">
     <div id="slide">
         <img class="d-block w-100" id="image" src="{{asset('img/no-ads.jpg')}}" alt="First slide">
-{{--        <h3>Tạo slideshow bằng HTML Và CSS</h3>--}}
-{{--        <div id="slideshow">--}}
-{{--            <div class="slide-wrapper">--}}
-{{--                <div class="slide"><img src="{{asset('img/no-ads.jpg')}}"></div>--}}
-{{--                <div class="slide"><img src="{{asset('img/youpip.jpg')}}"></div>--}}
-{{--                <div class="slide"><img src="{{asset('img/pip.jpg')}}"></div>--}}
-{{--                <div class="slide"><img src="{{asset('img/share-felling.jpg')}}"></div>--}}
-{{--                <div class="slide"><img src="{{asset('img/chat.jpg')}}"></div>--}}
-{{--                <div class="slide"><img src="https://images.pexels.com/photos/4484184/pexels-photo-4484184.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"></div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
     </div>
     <footer id="footer">
         <a href="https://play.google.com/store/apps/details?id=org.youpip.app&hl=vi&gl=US" target="_blank">
@@ -53,7 +51,6 @@
         </a>
     </footer>
 </div>
-<script src="https://unpkg.com/@popperjs/core@2">
 
 </script>
 <script>
@@ -61,7 +58,7 @@
         console.log("YouPiP")
         setTimeout(function () {
             console.log("YouPiP redirect")
-            window.location.href = 'https://play.google.com/store/apps/details?id=org.youpip.app&hl=vi&gl=US'
+            // window.location.href = 'https://play.google.com/store/apps/details?id=org.youpip.app&hl=vi&gl=US'
         }, 2000)
     })
 </script>

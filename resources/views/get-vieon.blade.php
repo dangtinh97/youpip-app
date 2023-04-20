6c0ced7ca869d223078f593f602b7287063f6c74
Dang Tinh <>
2023-04-20 20:38:52 +0700

    proxy config db

--- resources/views/get-vieon.blade.php
+++ resources/views/get-vieon.blade.php
@@ -7,6 +7,9 @@
     <title>GET VIEON</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <style>
+        body{
+            background: #efefef;
+        }
         pre {
             background-color: ghostwhite;
             border: 1px solid silver;
@@ -31,9 +34,7 @@
         <button data-url="https://vieon.vn/truyen-hinh-truc-tuyen/vtv2-hd/" class="btn btn-primary _get_vieon">VTV 2</button>
         <button data-url="https://vieon.vn/truyen-hinh-truc-tuyen/vtv3-hd/" class="btn btn-primary _get_vieon">VTV 3</button>
     </div>
-    <pre id="resultCode">
-        <code id=account></code>
-    </pre>
+    <pre id="resultCode"><code id=account></code></pre>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 <script

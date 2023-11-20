3bf481cc72914667681e3fe352cd806c9f1090a0
tinhvd <dangtinha2@gmail.com>
2023-11-20 15:22:19 +0700

    add money

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('command:curl-locamos')->everyMinute();
+        $schedule->command('command:curl-locamos')->everyFiveMinutes();
         $schedule->command('command:team-eat-lunch')->dailyAt('05:40');
     }
 
